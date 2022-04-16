d1494918635147c4754d2584dfe6ae10f282135f
Gil Tayar <gil@tayar.org>
2022-04-16 18:35:55 +0300

    lib-cjs with use-cjs working

---
+++ use-lib/src/use-cjs.js
@@ -0,0 +1,17 @@
+'use strict'
+const fs = require('fs/promises')
+const os = require('os')
+const path = require('path')
+const {makeDatabase} = require('json-database')
+
+async function main() {
+  const tmpDir = await fs.mkdtemp(os.tmpdir() + '/')
+
+  const db = makeDatabase(path.join(tmpDir, 'db.json'))
+
+  await db.addRow({name: 'a'})
+
+  console.log(await db.getRows())
+}
+
+main().catch(console.error)
